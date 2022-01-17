1bb777aab7a1f249ab8d092748f96f6ea88d009a
ChanpisethThon <97521185+ChanpisethThon@users.noreply.github.com>
2022-01-18 02:00:04 +0700

    Authentication
    Admin/User Auth

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,3 +27,17 @@ route::get('/', HomeComponent::class);
 route::get('/shop', ShopComponent::class);
 route::get('/cart', CartComponent::class);
 route::get('/checkout', CheckoutComponent::class);
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+
+//For user or customer
+route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+});
+
+//For Admin
+route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () {
+    route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+});
